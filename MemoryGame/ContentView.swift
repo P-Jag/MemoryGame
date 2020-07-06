1e4b6dc39379100ad0411fe1ec4f171be8049b48
P-Jag <pavel.jagla@gmail.com>
2020-07-06 15:08:00 +0200

    basic setup

---
+++ MemoryGame/ContentView.swift
@@ -0,0 +1,47 @@
+//
+//  ContentView.swift
+//  MemoryGame
+//
+//  Created by Paweł Jagła on 06/07/2020.
+//  Copyright © 2020 Paweł Jagła. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        HStack {
+            ForEach(0..<4) { index in
+                CardView(isFaceUp: false)
+            }
+        }
+        .padding()
+        .foregroundColor(Color.orange)
+        .font(Font.largeTitle)
+        
+    }
+}
+
+struct CardView: View {
+    
+    var isFaceUp: Bool
+    var body: some View {
+        
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+                Text("👻")
+            } else {
+                RoundedRectangle(cornerRadius: 10.0).fill()
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
