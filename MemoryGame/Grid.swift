639c36ade962438f61076fe4d6146ae12efa696c
P-Jag <pavel.jagla@gmail.com>
2020-07-08 14:50:44 +0200

    cards matching

--- MemoryGame/Grid.swift
+++ MemoryGame/Grid.swift
@@ -1,11 +1,3 @@
-//
-//  Grid.swift
-//  MemoryGame
-//
-//  Created by Paweł Jagła on 08/07/2020.
-//  Copyright © 2020 Paweł Jagła. All rights reserved.
-//
-
 import SwiftUI
 
 struct Grid<Item, ItemView>: View where Item: Identifiable, ItemView: View {
@@ -31,18 +23,10 @@ struct Grid<Item, ItemView>: View where Item: Identifiable, ItemView: View {
     }
     
     func body(for item: Item, in layout: GridLayout) -> some View {
-        let index = self.index(of: item)
+        let index = items.firstIndex(matching: item)!
+        
         return viewForItem(item)
             .frame(width: layout.itemSize.width, height: layout.itemSize.height)
             .position(layout.location(ofItemAt: index))
-    }
-    
-    func index(of item: Item) -> Int {
-        for index in 0..<items.count {
-            if items[index].id == item.id {
-                return index
-            }
-        }
-        return 0 // TODO: bogus!
     }
 }
--- MemoryGame/MemoryGame.swift
+++ MemoryGame/MemoryGame.swift
@@ -1,22 +1,31 @@
 import Foundation
 
-struct MemoryGame<CardContent> {
+struct MemoryGame<CardContent> where CardContent: Equatable {
     
     var cards: Array<Card>
     
-    mutating func choose(card: Card) {
-        print("Card chosen: \(card)")
-        let chosenIndex: Int = self.index(of: card)
-        self.cards[chosenIndex].isFaceUp = !self.cards[chosenIndex].isFaceUp
-    }
-    
-    func index(of card: Card) -> Int {
-        for index in 0..<cards.count {
-            if self.cards[index].id == card.id {
-                return index
+    var indexOfOneAndOnlyFaceUpCard: Int? {
+        get { cards.indices.filter { cards[$0].isFaceUp}.only }
+        set {
+            for index in cards.indices {
+                cards[index].isFaceUp = index == newValue
             }
         }
-        return 0 // TODO: bogus!
+    }
+    
+    mutating func choose(card: Card) {
+        
+        if let chosenIndex: Int = cards.firstIndex(matching: card), !cards[chosenIndex].isFaceUp, !cards[chosenIndex].isMatched {
+            if let potentialMatchIndex = indexOfOneAndOnlyFaceUpCard {
+                if cards[chosenIndex].content == cards[potentialMatchIndex].content {
+                    cards[chosenIndex].isMatched = true
+                    cards[potentialMatchIndex].isMatched = true
+                }
+                self.cards[chosenIndex].isFaceUp = true
+            } else {
+                indexOfOneAndOnlyFaceUpCard = chosenIndex
+            }
+        }
     }
     
     init(numberOfpairsOfCards: Int, cardContentFactory: (Int) -> CardContent) {
@@ -28,10 +37,9 @@ struct MemoryGame<CardContent> {
         }
     }
     
-    
     //MemoryGame.Card
     struct Card: Identifiable {
-        var isFaceUp: Bool = true
+        var isFaceUp: Bool = false
         var isMatched: Bool = false
         var content: CardContent
         var id: Int
